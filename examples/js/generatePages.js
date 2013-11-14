1c649a2cfa34c77b0082061b8938b7c02a7ea944
getbackwards <getbackwards@gmail.com>
2013-11-15 10:10:59 +1300

    working on mosaic styling

---
+++ examples/js/generatePages.js
@@ -0,0 +1,74 @@
+//$(document).ready(function(){
+
+jQuery(function($) {
+	function generatePages() {
+		var _total, i, _link;
+
+		_total = $( "#carousel" ).rcarousel( "getTotalPages" );
+
+		for ( i = 0; i < _total; i++ ) {
+			_link = $( "<a href='#'></a>" );
+
+			$(_link)
+			.bind("click", {page: i},
+				function( event ) {
+					$( "#carousel" ).rcarousel( "goToPage", event.data.page );
+					event.preventDefault();
+				}
+				)
+			.addClass( "bullet off" )
+			.appendTo( "#pages" );
+		}
+
+				// mark first page as active
+				$( "a:eq(0)", "#pages" )
+				.removeClass( "off" )
+				.addClass( "on" )
+				.css( "background-image", "url(images/page-on.png)" );
+
+			}
+
+			function pageLoaded( event, data ) {
+				$( "a.on", "#pages" )
+				.removeClass( "on" )
+				.css( "background-image", "url(images/page-off.png)" );
+
+				$( "a", "#pages" )
+				.eq( data.page )
+				.addClass( "on" )
+				.css( "background-image", "url(images/page-on.png)" );
+			}
+			
+			$("#carousel").rcarousel(
+			{
+				visible: 1,
+				step: 1,
+				speed: 700,
+				auto: {
+					enabled: false
+				},
+				//width: 780,
+				//width: 855,
+				width: 850,
+				//height: 240,
+				//height: 286,
+				height: 304,
+				start: generatePages,
+				pageLoaded: pageLoaded
+			}
+			);
+			
+			$( "#ui-carousel-next" )
+			.add( "#ui-carousel-prev" )
+			.add( ".bullet" )
+			.hover(
+				function() {
+					$( this ).css( "opacity", 0.7 );
+				},
+				function() {
+					$( this ).css( "opacity", 1.0 );
+				}
+				);
+		});
+
+//});
