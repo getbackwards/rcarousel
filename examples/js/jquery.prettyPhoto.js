179d5378e3550267d177fc80c21b170d7e667b02
getbackwards <getbackwards@gmail.com>
2013-11-19 23:54:29 +1300

    Don't want to jinx it ...
    but might be getting somewhere with combining rcarousel with prettyPhoto

--- examples/js/jquery.prettyPhoto.js
+++ examples/js/jquery.prettyPhoto.js
@@ -5,27 +5,27 @@
 	Version: 3.1.5
 ------------------------------------------------------------------------- */
 
-$(document).ready(function(){
+//$(document).ready(function(){
 
 (function($) {
 	$.prettyPhoto = {version: '3.1.5'};
 	
 	$.fn.prettyPhoto = function(pp_settings) {
 		pp_settings = jQuery.extend({
-			hook: 'rel', /* the attribute tag to use for prettyPhoto hooks. default: 'rel'. For HTML5, use "data-rel" or similar. */
+			hook: 'data-rel', /* the attribute tag to use for prettyPhoto hooks. default: 'rel'. For HTML5, use "data-rel" or similar. */
 			animation_speed: 'fast', /* fast/slow/normal */
 			ajaxcallback: function() {},
 			slideshow: 5000, /* false OR interval time in ms */
 			autoplay_slideshow: false, /* true/false */
-			opacity: 0.80, /* Value between 0 and 1 */
+			opacity: 1, /* Value between 0 and 1 */
 			show_title: true, /* true/false */
 			allow_resize: true, /* Resize the photos bigger than viewport. true/false */
-			allow_expand: true, /* Allow the user to expand a resized image. true/false */
+			allow_expand: false, /* Allow the user to expand a resized image. true/false */
 			default_width: 500,
 			default_height: 344,
 			counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
-			theme: 'pp_default', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
-			horizontal_padding: 20, /* The padding on each side of the picture */
+			theme: 'dark_square', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
+			horizontal_padding: 0, /* The padding on each side of the picture */
 			hideflash: false, /* Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto */
 			wmode: 'opaque', /* Set the flash wmode attribute */
 			autoplay: true, /* Automatically start videos: True/False */
@@ -913,4 +913,4 @@ $(document).ready(function(){
 
 var pp_alreadyInitialized = false; // Used for the deep linking to make sure not to call the same function several times.
 
-});
+//});
